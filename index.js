559ef5266e4b96997813f9bc9e6fa427159eedfd
夕舞八弦 <1677759063@qq.com>
2020-10-17 10:31:35 +0800

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const getRawBody = require("raw-body");
+const Core = require("@alicloud/pop-core");
+const queryString = require("query-string");
+exports.handler = (req, resp, context) => {
+    getRawBody(req, function (err, body) {
+        body = queryString.parse(body.toString());
+        if (body.token !== "<token>") {
+            resp.send('wrong token');
+            return;
+        }
+        console.log(body.path);
+        var client = new Core({
+            accessKeyId: context.credentials.accessKeyId,
+            accessKeySecret: context.credentials.accessKeySecret,
+            securityToken: context.credentials.securityToken,
+            endpoint: "https://cdn.aliyuncs.com",
+            apiVersion: "2018-05-10",
+        });
+        client
+            .request(
+                "RefreshObjectCaches",
+                {
+                    RegionId: "cn-hangzhou",
+                    ObjectPath: body.path,
+                },
+                {
+                    method: "POST",
+                }
+            )
+            .then(
+                (result) => {
+                    console.log(JSON.stringify(result));
+                    resp.send(JSON.stringify(result, null, "    "));
+                },
+                (ex) => {
+                    console.log(ex);
+                    resp.send(JSON.stringify(ex, null, "    "));
+                }
+            );
+    });
+};
